11a1d43d9c5f4ce86036e336d3ab885cfb634014
harshit0571 <harshitchadha65@gmail.com>
2023-08-02 21:28:29 +0530

    added search query url feature

--- components/Search_Input.tsx
+++ components/Search_Input.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { ChangeEventHandler, useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/use-debounce";
+import qs from "query-string";
 
 const Search_Input = () => {
   const router = useRouter();
@@ -14,10 +16,37 @@ const Search_Input = () => {
 
   const [Value, setValue] = useState(name || "");
 
+  const debouncedValue = useDebounce<string>(Value, 500);
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    console.log(e.target.value);
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const query = {
+      name: debouncedValue,
+      categoryId: categoryId,
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(url);
+  }, [debouncedValue, router, categoryId]);
   return (
     <div className="relative">
       <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
-      <Input placeholder="search..." className="pl-10 bg-primary/10" />
+      <Input
+        placeholder="search..."
+        className="pl-10 bg-primary/10"
+        value={Value}
+        onChange={onChange}
+      />
     </div>
   );
 };
